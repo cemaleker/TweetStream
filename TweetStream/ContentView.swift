4bfed7e73052f8233d8df9217407699264f2046d
Cemal Eker <cemaleker@gmail.com>
2021-11-04 17:47:59 +0100

    Initial Commit

---
+++ TweetStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TweetStream
+//
+//  Created by Cemal Eker on 04.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TweetStream/TweetStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetStreamApp.swift
+//  TweetStream
+//
+//  Created by Cemal Eker on 04.11.21.
+//
+
+import SwiftUI
+
+@main
+struct TweetStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
